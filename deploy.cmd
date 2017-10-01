9391bed7d7cbfc36d62f274024a4823d3ac78181
spencersa <spencersa715@gmail.com>
2017-10-01 15:45:09 -0500

    remove azure scripts

--- deploy.cmd
+++
@@ -1,9 +0,0 @@
-
-if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  install bower
-  exitWithMessageOnError "installing bower failed"
-  ./node_modules/.bin/bower install
-  exitWithMessageOnError "bower failed"
-  cd - > /dev/null
-fi
